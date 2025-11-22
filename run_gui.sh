23ad6b2dc5ebc3e7cb3e203cd1c978c1680c4fde
Derin Gezgin <dgezgin@conncoll.edu>
2025-11-22 12:46:37 -0500

    Update run_gui.sh

--- run_gui.sh
+++ run_gui.sh
@@ -4,16 +4,18 @@ MODE="${1:-}"
 echo "Adding the Python bindings to PYTHONPATH" 
 export PYTHONPATH="${PYTHONPATH:-}:$(pwd)/planet-wars-rts/app/src/main/python"
 
-echo "Killing the existing server"
-pkill -f "client_server/game_agent_server.py" 2>/dev/null || true
-
-echo "Starting the new server"
-python3 planet-wars-rts/app/src/main/python/client_server/game_agent_server.py &
-sleep 5
-
-cd planet-wars-rts
 if [ "$MODE" = "headless" ]; then
-  xvfb-run ./gradlew :app:runGUI
+  echo "Running the agents in the headless mode"
+  python3 run_agents.py
 else
+  echo "Killing the existing server"
+  pkill -f "client_server/game_agent_server.py" 2>/dev/null || true
+
+  echo "Starting the new server"
+  python3 planet-wars-rts/app/src/main/python/client_server/game_agent_server.py &
+  sleep 5
+
+  echo "Running the agents"
+  cd planet-wars-rts
   ./gradlew :app:runGUI
 fi
