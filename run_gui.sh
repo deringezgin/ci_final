63332663d656a90d91a8d6d8f4d1d6738a374047
Derin Gezgin <dgezgin@conncoll.edu>
2025-11-22 12:01:46 -0500

    Adding Update to the running script.

--- run_gui.sh
+++ run_gui.sh
@@ -1,9 +1,13 @@
 set -euo pipefail
 
 MODE="${1:-}"
+echo "Adding the Python bindings to PYTHONPATH" 
+export PYTHONPATH="${PYTHONPATH:-}:$(pwd)/planet-wars-rts/app/src/main/python"
 
+echo "Killing the existing server"
 pkill -f "client_server/game_agent_server.py" 2>/dev/null || true
 
+echo "Starting the new server"
 python3 planet-wars-rts/app/src/main/python/client_server/game_agent_server.py &
 sleep 5
 
